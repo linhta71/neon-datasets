908fc76ce18bb965190621ba0322adc2e6e05e50
Kristina Riemer <kristina.riemer@weecology.org>
2020-12-18 07:10:21 -0700

    Expand phenology data to all sites and all dates

--- phenology/curate_data.R
+++ phenology/curate_data.R
@@ -11,11 +11,8 @@ if(!dir.exists(paste0(download_loc, cover_product_id))){
 }
 
 ###########Clean up data###########
-
 status_intensity <- neon_read(table = "phe_statusintensity-basic", 
-                                    site = "DSNY", 
-                                    start_date = "2018-03-01", 
-                                    end_date = "2018-12-30")
+                              product = cover_product_id)
 
 pheno_records <- status_intensity %>% 
   select(uid, plotID, date, individualID, phenophaseName, phenophaseStatus, 
@@ -24,9 +21,23 @@ pheno_records <- status_intensity %>%
          phenophaseIntensity == "50-74%") %>% 
   rename(uid_pheno = uid)
 
-per_individual <- neon_read(table = "phe_perindividual-basic", 
-                            site = "DSNY")
+sites <- c("ABBY", "BARR", "BART", "BLAN", "BONA", "CLBJ", "CPER", "DCFS", "DEJU", "DELA", 
+           "DSNY", "GRSM", "GUAN", "HARV", "HEAL", "JERC", "JORN", "KONA", "KONZ", "LAJA", 
+           "LENO", "MLBS", "MOAB", "NIWO", "NOGP", "OAES", "ONAQ", "ORNL", "OSBS", "PUUM", 
+           "RMNP", "SCBI", "SERC", "SJER", "SOAP", "SRER", "STEI", "STER", "TALL", "TEAK", 
+           "TOOL", "TREE", "UKFS", "UNDE", "WOOD", "WREF", "YELL")
 
+per_individual <- c()
+for (site in sites) {
+  per_ind_by_site <- neon_read(table = "phe_perindividual-basic", 
+                               product = cover_product_id, 
+                               site = site)
+  per_ind_by_site <- select(per_ind_by_site, uid, decimalLatitude, 
+                            decimalLongitude, individualID, scientificName, siteID)
+  per_individual <- rbind(per_ind_by_site, per_individual)
+}
+
+# TODO: figure out why some individualIDs have multiple species
 checking_duplicates <- per_individual %>% 
   group_by(individualID) %>% 
   summarise(id_occurrences = n_distinct(scientificName), 
@@ -35,19 +46,19 @@ checking_duplicates <- per_individual %>%
   filter(id_occurrences > 1 | lat_occurrences > 1 | lon_occurrences > 1)
 
 individual_records <- per_individual %>% 
-  select(uid, decimalLatitude, decimalLongitude, individualID, scientificName) %>% 
   group_by(individualID) %>% 
   slice(1) %>% 
   ungroup() %>% 
   rename(uid_ind = uid)
 
 combined <- left_join(pheno_records, individual_records, by = "individualID") %>% 
-  group_by(individualID) %>% 
+  tidyr::separate(date, c("year", "month", "day"), sep = "-", remove = FALSE) %>% 
+  group_by(individualID, year) %>% 
   arrange(date) %>% 
   slice(1) %>% 
   ungroup() %>% 
-  select(scientificName, decimalLatitude, decimalLongitude, plotID, date, 
-         uid_pheno, uid_ind) %>% 
+  select(individualID, scientificName, decimalLatitude, decimalLongitude, plotID, 
+         date, uid_pheno, uid_ind) %>% 
   rename(species = scientificName, lat = decimalLatitude, lon = decimalLongitude, 
          sitename = plotID, first_flower_date = date)
 
