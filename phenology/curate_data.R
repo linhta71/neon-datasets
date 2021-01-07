c84e5c48a8a89e71f0607f7ecc9c8c9bab0dc741
Kristina Riemer <kristina.riemer@weecology.org>
2021-01-07 13:11:24 -0700

    Add NEON precip data to phenology dataset

--- phenology/curate_data.R
+++ phenology/curate_data.R
@@ -74,5 +74,40 @@ species_list_comp <- c(species_list1, species_list2)
 combined_subset <- combined %>% 
   filter(grepl(paste(species_list_comp, collapse = "|"), species))
 
+###########Add weather data###########
+pheno_sites <- combined_subset %>% 
+  tidyr::separate(sitename, c("Site", "Transect"), sep = "_") %>% 
+  select(Site) %>% 
+  summarize(unique(Site))
+pheno_sites <- as.vector(pheno_sites$`unique(Site)`)
+
+precip_product_id <- "DP1.00006.001"
+if(!dir.exists(paste0(download_loc, precip_product_id))){
+  neon_download(product = precip_product_id, type = "expanded")
+}
+
+precip <- neon_read(table = "SECPRE_30min-expanded", 
+                    product = precip_product_id, 
+                    site = pheno_sites[pheno_sites != "SRER"])
+
+mean_daily_precip <- precip %>% 
+  filter(!is.na(secPrecipBulk)) %>% 
+  tidyr::separate(startDateTime, c("startDate", "startTime"), sep = " ") %>% 
+  group_by(siteID, startDate) %>% 
+  summarize(count = n(), daily_precip = sum(secPrecipBulk)) %>% 
+  filter(count == 48) %>% 
+  mutate(year = substr(startDate, 1, 4)) %>% 
+  group_by(siteID, year) %>% 
+  summarize(count = n(), mean_daily_precip = mean(daily_precip)) %>% 
+  filter(count %in% c(364, 365, 366)) %>% 
+  select(-count)
+
+combined_subset <- combined_subset %>% 
+  mutate(site = substr(sitename, 1, 4), 
+         year = substr(first_flower_date, 1, 4))
+
+combined_subset <- left_join(combined_subset, mean_daily_precip, 
+                                    by = c("site" = "siteID", "year" = "year"))
+
 ###########Save data###########
 write.csv(combined_subset, file = "phenology/phenology.csv", row.names = FALSE)
