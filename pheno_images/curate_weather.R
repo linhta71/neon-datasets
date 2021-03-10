1c6b4bb5d398b4fd9d95fd22da16a8b5f8ee93b0
Kristina Riemer <kristina.riemer@weecology.org>
2021-03-10 13:39:23 -0700

    Add code to add NOAA weather data for GCC NEON sites

--- pheno_images/curate_weather.R
+++ pheno_images/curate_weather.R
@@ -1,6 +1,7 @@
 ###########Read in libraries###########
 library(daymetr)
 library(dplyr)
+library(ggplot2)
 
 ###########Download weather data###########
 download.file("https://www.neonscience.org/sites/default/files/NEON_Field_Site_Metadata_20201204.csv", 
@@ -22,6 +23,52 @@ for(site in 1:nrow(efi_sites)){
     mutate(site = raw_daymet$site)
   all_daymet <- bind_rows(df_daymet, all_daymet)
 }
+
+# get api key here: https://www.ncdc.noaa.gov/cdo-web/token
+library(rnoaa)
+
+all_stations <- ghcnd_stations()
+close_stations <- all_stations %>% 
+  filter(latitude > min(efi_sites$field_latitude) & 
+           latitude < max(efi_sites$field_latitude) &
+           longitude > min(efi_sites$field_longitude) & 
+           longitude < max(efi_sites$field_longitude))
+site <- 1
+num <- 0.07
+single_close_station <- all_stations %>% 
+  filter(latitude > efi_sites$field_latitude[site] - num & 
+           latitude < efi_sites$field_latitude[site] + num &
+           longitude > efi_sites$field_longitude[site] - num & 
+           longitude < efi_sites$field_longitude[site] + num)
+
+efi_ghcnd <- all_stations %>% 
+  filter(name %in% c("BARTLETT", "HARVARD FOREST", 
+                     "FRONT ROYAL 3.1 ESE", "RHINELANDER ONEIDA AP", 
+                     "KU-FIELD STN", "GATLINBURG 2 SW", 
+                     "DEMOPOLIS LOCK 4", "FORESTBURG 5 S")) %>% 
+  group_by(name) %>% 
+  slice_tail(n = 1)
+
+
+map_background <- map_data("state") 
+
+ggplot() +
+  geom_polygon(data = map_background, aes(x = long, y = lat, group = group), 
+               fill = "white", color = "black") + 
+  geom_point(data = efi_sites, aes(x = field_longitude, y = field_latitude), 
+             color = "blue") +
+  geom_point(data = efi_ghcnd, aes(x = longitude, y = latitude), 
+             color = "red", shape = 4) +
+  labs(x = "", y = "") +
+  theme_classic()
+
+
+one_site_data <- ncdc(datasetid = "GHCND", stationid = "GHCND:USC00270390", 
+                      startdate = "2018-01-01", enddate = "2018-12-01")
+
+ncdc_stations(datasetid='GHCND', locationid='GHCND:USC00270390', 
+              stationid='GHCND:USC00084289')
+
 
 ###########Clean up weather data###########
 clean_daymet <- all_daymet %>% 
