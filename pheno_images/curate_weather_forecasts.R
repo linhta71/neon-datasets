6b1f5aeed7a5762a06cd0abf65773bc21ecc0b5a
David LeBauer <dlebauer@arizona.edu>
2021-04-08 13:47:38 -0700

    add code to download NOAA GEFS forecasts from EFI

---
+++ pheno_images/curate_weather_forecasts.R
@@ -0,0 +1,8 @@
+# devtools::install_github('eco4cast/neon4cast')
+# install.packages('readr')
+library(neon4cast)
+pheno_sites <- c("HARV", "BART", "SCBI", "STEI", "UKFS", "GRSM", "DELA", "CLBJ"
+)
+download_noaa(pheno_sites)
+noaa_fc <- stack_noaa()
+readr::write_csv(noaa_fc, file = paste0('NOAA_GEFS_35d_', Sys.Date(), '.csv'))
