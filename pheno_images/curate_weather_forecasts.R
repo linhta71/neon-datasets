d91c0f5ed19a4bd692dd4635d1693121bd048b61
David LeBauer <dlebauer@arizona.edu>
2021-04-11 15:24:15 -0700

    Update pheno_images/curate_weather_forecasts.R
    Co-authored-by: Kristina Riemer <kristina.riemer@weecology.org>

--- pheno_images/curate_weather_forecasts.R
+++ pheno_images/curate_weather_forecasts.R
@@ -1,7 +1,7 @@
 # devtools::install_github('eco4cast/neon4cast')
 # install.packages('readr')
 library(neon4cast)
-pheno_sites <- c("HARV", "BART", "SCBI", "STEI", "UKFS", "GRSM", "DELA", "CLBJ"
+pheno_sites <- c("HARV", "BART", "SCBI", "STEI", "UKFS", "GRSM", "DELA", "CLBJ)"
 )
 download_noaa(pheno_sites)
 noaa_fc <- stack_noaa()
