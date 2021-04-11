6fa76a8c704063d8291f1d943cba4b7c20d4cdeb
David LeBauer <dlebauer@arizona.edu>
2021-04-11 15:24:37 -0700

    Update pheno_images/curate_weather_forecasts.R
    Co-authored-by: Kristina Riemer <kristina.riemer@weecology.org>

--- pheno_images/curate_weather_forecasts.R
+++ pheno_images/curate_weather_forecasts.R
@@ -2,7 +2,6 @@
 # install.packages('readr')
 library(neon4cast)
 pheno_sites <- c("HARV", "BART", "SCBI", "STEI", "UKFS", "GRSM", "DELA", "CLBJ)"
-)
 download_noaa(pheno_sites)
 noaa_fc <- stack_noaa()
 readr::write_csv(noaa_fc, file = paste0('pheno_images/NOAA_GEFS_35d_', Sys.Date(), '.csv'))
